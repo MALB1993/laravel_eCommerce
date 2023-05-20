7fb1d935b4c6b4bec067967b24fb5b8718a884b2
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-20 10:25:52 +0330

    created user address migarion and fixed structure

---
+++ database/migrations/2023_05_20_101513_create_user_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->string('address');
+            $table->string('cellphone');
+            $table->string('postal_cod');
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->bigInteger('province_id');
+            $table->bigInteger('city_id');
+
+            $table->string('longitude')->nullable();
+            $table->string('latitude')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+};

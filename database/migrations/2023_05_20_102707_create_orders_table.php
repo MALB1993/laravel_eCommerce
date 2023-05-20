fec93eb73a1422ab2b8db089595ffcfd27d5014b
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-20 10:51:55 +0330

    created orders  migarion and fixed structure

---
+++ database/migrations/2023_05_20_102707_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->foreignId('address_id');
+            $table->foreign('address_id')->references('id')->on('user_addresses')->onDelete('cascade');
+
+            $table->foreignId('coupon_id')->nullable();
+            $table->foreign('coupon_id')->references('id')->on('coupons')->onDelete('cascade');
+
+            $table->tinyInteger('status')->default(0);
+            $table->unsignedInteger('total_amount');
+            $table->unsignedInteger('delivery_amount')->default(0);
+            $table->unsignedInteger('coupon_amount')->default(0);
+            $table->unsignedInteger('paying_amount');
+            $table->enum('payment_type',['pos', 'cash', 'shabaMumber', 'cardToCard', 'online']);
+            $table->tinyInteger('payment_status')->default(0);
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};

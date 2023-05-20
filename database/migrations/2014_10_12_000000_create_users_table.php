3caa4c427154b32acfc7b6267dfd4296661fc625
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-20 09:24:06 +0330

    modified users migration and added new cloumns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('avatar');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cellphone')->unique();
+            $table->string('provider_name');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

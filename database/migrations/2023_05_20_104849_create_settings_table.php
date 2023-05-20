97cf9bb9912e6dcdc8657955e99abfec266a27ca
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-20 10:54:38 +0330

    created settings  migarion and fixed structure

---
+++ database/migrations/2023_05_20_104849_create_settings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('logo');
+
+            $table->string('address');
+            $table->string('cellphone');
+            $table->string('cellphone_tow')->nullable();
+
+            $table->string('longitude')->nullable();
+            $table->string('latitude')->nullable();
+
+            $table->string('instagram')->nullable();
+            $table->string('telegram')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('facebook')->nullable();
+
+            $table->text('footer')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};

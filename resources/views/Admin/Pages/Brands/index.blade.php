3ffe09b953727f94883df817d8576c1b9674aacb
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-21 10:41:13 +0330

    added structure

---
+++ resources/views/Admin/Pages/Brands/index.blade.php
@@ -0,0 +1,16 @@
+@extends('Admin.partials.master')
+@section('title', 'ایجاد برند جدید')
+@section('content')
+
+<!-- Content Row -->
+<div class="row">
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-12 col-md-12 mb-4 bg-white my-4 p-4">
+        <div class="mb-4">
+            <h5 class="font-weight-bold">برندها</h5>
+        </div>
+        <hr>
+    </div>
+</div>
+
+@endsection

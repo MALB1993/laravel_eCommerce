56c71cc4b5028f270d4e29dc2102ba23f9983cda
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-21 12:45:52 +0330

    fixed sidebar

--- resources/views/Admin/sections/sidebar.blade.php
+++ resources/views/Admin/sections/sidebar.blade.php
@@ -13,102 +13,22 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-      <a class="nav-link" href="index.html">
+      <a class="nav-link" href="{{ route('dashboard') }}">
         <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span> داشبورد </span></a>
+        <span> {{ __('داشبورد') }} </span></a>
     </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-      لورم ایپسوم
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Nav Item - Brands -->
     <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-        aria-controls="collapseTwo">
-        <i class="fas fa-fw fa-cog"></i>
-        <span> کامپونت ها </span>
-      </a>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header"> کامپونت سفارشی : </h6>
-          <a class="collapse-item" href="#">Buttons</a>
-          <a class="collapse-item" href="#">Cards</a>
-        </div>
-      </div>
-    </li>
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-        aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-wrench"></i>
-        <span> ابزار ها </span>
-      </a>
-      <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header"> لورم ایپسوم </h6>
-          <a class="collapse-item" href="#">Colors</a>
-          <a class="collapse-item" href="#">Borders</a>
-          <a class="collapse-item" href="#">Animations</a>
-          <a class="collapse-item" href="#">Other</a>
-        </div>
-      </div>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-      لورم
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-        aria-controls="collapsePages">
-        <i class="fas fa-fw fa-folder"></i>
-        <span> صفحات </span>
-      </a>
-      <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header"> صفحات ورود : </h6>
-          <a class="collapse-item" href="login.html"> ورود </a>
-          <a class="collapse-item" href="register.html"> عضویت </a>
-          <a class="collapse-item" href="forgot-password.html"> فراموشی رمز عبور </a>
-          <div class="collapse-divider"></div>
-          <h6 class="collapse-header"> صفحات دیگر : </h6>
-          <a class="collapse-item" href="404.html">404 Page</a>
-          <a class="collapse-item" href="#">Blank Page</a>
-        </div>
-      </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-      <a class="nav-link" href="#">
-        <i class="fas fa-fw fa-chart-area"></i>
-        <span> نمودار ها </span></a>
-    </li>
-
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-      <a class="nav-link" href="#">
-        <i class="fas fa-fw fa-table"></i>
-        <span> جداول </span></a>
+      <a class="nav-link" href="{{ route('admin.brands.index') }}">
+        <i class="fas fa-store"></i>
+        <span> {{ __('برندها') }} </span></a>
     </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-      <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
 
   </ul>

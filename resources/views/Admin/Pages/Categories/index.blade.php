85cb6bfcd878c26f6177a1cc44c4d12030755381
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-22 20:20:24 +0330

    added view categories

---
+++ resources/views/Admin/Pages/Categories/index.blade.php
@@ -0,0 +1,15 @@
+@extends('Admin.partials.master')
+@section('title')
+    {{ __('دسته بندی ها') }}
+@endsection
+@section('content')
+
+<!-- Content Row -->
+<div class="row">
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-12 col-md-12 mb-4 bg-white my-4 p-4">
+
+    </div>
+</div>
+
+@endsection

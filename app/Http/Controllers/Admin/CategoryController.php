2af544a5beacbb7ee233c8c1b1f4d55be9b7dda8
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-23 12:33:56 +0330

    fixed update method

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,6 +12,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
@@ -70,6 +71,7 @@ class CategoryController extends Controller
                 'is_active'     =>         $storeCategoryRequest->input('is_active'),
                 'description'   =>         $storeCategoryRequest->input('description'),
                 'icon'          =>         $storeCategoryRequest->input('icon'),
+                'updated_at'    =>         null
             ]);
 
             foreach ($storeCategoryRequest->input('attribute_ids') as $attributeId) {
@@ -78,6 +80,8 @@ class CategoryController extends Controller
                 $attribute->categories()->attach($category->id, [
                     'is_filter'     =>      in_array($attributeId, $storeCategoryRequest->input('attribute_is_filter_ids')) ? 1 : 0,
                     'is_variation'  =>      $storeCategoryRequest->input('variation_id') === $attributeId ? 1 : 0,
+                    'created_at'    =>      Carbon::now(),
+                    'updated_at'    =>      null
                 ]);
             }
 
@@ -141,7 +145,60 @@ class CategoryController extends Controller
      */
     public function update(UpdateCategoryRequest $updateCategoryRequest, Category $category)
     {
-        dd($updateCategoryRequest->all());
+
+
+        #______________________________________ try and catch for create category and pivot table
+        try {
+            #_______________________ Start transaction database
+            DB::beginTransaction();
+
+            $category->update([
+                'parent_id'     =>         $updateCategoryRequest->input('parent_id'),
+                'name'          =>         $updateCategoryRequest->input('name'),
+                'slug'          =>         $updateCategoryRequest->input('slug'),
+                'is_active'     =>         $updateCategoryRequest->input('is_active'),
+                'description'   =>         $updateCategoryRequest->input('description'),
+                'icon'          =>         $updateCategoryRequest->input('icon'),
+                'updated_at'    =>         Carbon::now()
+            ]);
+
+            $category->attributes()->detach();
+            foreach ($updateCategoryRequest->input('attribute_ids') as $attributeId) {
+                $attribute =  Attribute::query()->findOrFail($attributeId);
+
+                $attribute->categories()->attach($category->id, [
+                    'is_filter'     =>      in_array($attributeId, $updateCategoryRequest->input('attribute_is_filter_ids')) ? 1 : 0,
+                    'is_variation'  =>      $updateCategoryRequest->input('variation_id') === $attributeId ? 1 : 0,
+                    'updated_at'    =>      Carbon::now()
+                ]);
+            }
+
+            DB::commit();
+            #_______________________ End transaction database
+
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            #_________________________________________ variables
+            $message = 'مشکل در ویرایش دسته بندی ها به وجود آمده است .';
+
+            #_________________________________________ Sweet Alert
+            alert()->error($message, $exception->getMessage())->persistent('متوجه شدم');
+
+            #_________________________________________ pass message and redirect
+            return redirect()->back();
+
+        }
+
+        #_________________________________________[ if every thing passed ]
+
+        #_________________________________________ variables
+        $message = 'دسته بندی شما به ویرایش ذخیره شد';
+
+        #_________________________________________ Sweet Alert
+        alert()->success('گزارش وضعیت', $message);
+
+        #_________________________________________ pass message and redirect
+        return redirect()->route('admin.categories.index');
     }
 
     /**

e5e15413362cb074175e6143cd722a29dfbac77d
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-24 14:38:40 +0330

    modified model attributes

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 
+/**
+ * @method static findOrFail(mixed $attributeId)
+ */
 class Attribute extends Model
 {
     use HasFactory, SoftDeletes;

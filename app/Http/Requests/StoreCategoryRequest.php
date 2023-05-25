84a7da02868d8dd0dbd150f976ad4c49fc27ff2f
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-25 20:07:52 +0330

    fixed bug validate

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -20,17 +20,18 @@ class StoreCategoryRequest extends FormRequest
      *
      * @return array
      */
-    #[ArrayShape(['name' => "string[]", 'slug' => "string[]", 'attribute_ids' => "string[]", 'attribute_is_filter_ids' => "string[]", 'variation_id' => "string[]", 'icon' => "string[]", 'description' => "string[]"])]
+    #[ArrayShape(['name' => "string[]",'is_active' => "string[]", 'slug' => "string[]", 'attribute_ids' => "string[]", 'attribute_is_filter_ids' => "string[]", 'variation_id' => "string[]", 'icon' => "string[]", 'description' => "string[]"])]
     public function rules(): array
     {
         return [
-            'name'                     =>   ['required','string','min:4','max:50'],
-            'slug'                     =>   ['required','min:4','max:50','unique:categories,slug'],
+            'name'                     =>   ['required','string','min:3','max:50'],
+            'slug'                     =>   ['required','min:3','max:50','unique:categories,slug'],
             'attribute_ids'            =>   ['required'],
             'attribute_is_filter_ids'  =>   ['required'],
             'variation_id'             =>   ['required','numeric'],
             'icon'                     =>   ['nullable','string'],
             'description'              =>   ['nullable'],
+            'is_active'                =>   ['required'],
         ];
     }
 }

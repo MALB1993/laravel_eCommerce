2f97a739f4c378ab289ccbf8e4cd09e11b585a20
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-25 22:04:30 +0330

    fiex some part show page product and fixed index view procuct

--- resources/views/Admin/Pages/Products/index.blade.php
+++ resources/views/Admin/Pages/Products/index.blade.php
@@ -11,14 +11,89 @@
         <div class="mb-4 d-flex justify-content-between">
             <h5 class="font-weight-bold">
                 {{ __('محصولات ها') }}
-{{--                ({{ $tags->total() }})--}}
+                ({{ $products->total() }})
             </h5>
             <a href="{{ route('admin.products.create') }}" class="btn btn-sm btn-primary">
                 <i class="fa fa-fw fa-plus"></i>
                 {{ __('ایجاد محصول') }}
             </a>
         </div>
-
+        <div>
+            <table class="table table-bordered text-center table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">{{ __('نام') }}</th>
+                    <th scope="col">{{ __('تصویر شاخص') }}</th>
+                    <th scope="col">{{ __('نام برند') }}</th>
+                    <th scope="col">{{ __('نام دسته') }}</th>
+                    <th scope="col">{{ __('وضعیت') }}</th>
+                    <th scope="col">{{ __('عملیات') }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($products as $key => $product)
+                    <tr>
+                        <th scope="row">{{ $products->firstItem() + $key }}</th>
+                        <td>
+                            <a href="{{ route('admin.products.show',['product' => $product->id]) }}">
+                                {{ $product->name }}
+                            </a>
+                        </td>
+                        <td>
+                            <img src="{{ asset('/upload/product/files/images'.'/'.$product->primary_image ) }}" alt="" width="100" height="100" class="rounded border border-dark">
+                        </td>
+                        <td>
+                            <a href="{{ route('admin.products.show',['product' => $product->id]) }}">
+                                {{ $product->brand->name }}
+                            </a>
+                        </td>
+                        <td>{{ $product->category->name }}</td>
+                        <td>
+                            {{-- add color if active text green or deactive text danger --}}
+                            <span class="{{ $product->getRawOriginal('is_active') ? 'text-success' : 'text-danger' }}">
+                            {{ $product->is_active }}
+                        </span>
+                        </td>
+                        <td>
+                            <!-- Example single danger button -->
+                            <div class="btn-group">
+                                <button
+                                    type="button"
+                                    class="btn btn-light dropdown-toggle"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false">
+                                    {{ __('عملیات') }}
+                                </button>
+                                <div class="dropdown-menu text-center p-3">
+                                    <a class="dropdown-item d-flex justify-content-between text-success" href="{{ route('admin.products.show', ['product' => $product->id]) }}">
+                                        <i class="fa fw-fw fa-eye"></i>
+                                        {{ __('جزئیات') }}
+                                    </a>
+                                    <a class="dropdown-item d-flex justify-content-between text-primary" href="{{ route('admin.products.edit', ['product' => $product->id]) }}">
+                                        <i class="fa fw-fw fa-pen"></i>
+                                        {{ __('ویرایش') }}
+                                    </a>
+                                    <form action="{{ route('admin.products.destroy', ['product' => $product->id]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="dropdown-item d-flex justify-content-between text-danger">
+                                            <i class="fa fw-fw fa-trash"></i>
+                                            {{ __('حذف') }}
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-content-center">
+            {{ $products->links('vendor\pagination\bootstrap-4') }}
+        </div>
     </div>
 </div>
 
--- resources/views/Admin/Pages/Products/show.blade.php
+++ resources/views/Admin/Pages/Products/show.blade.php
@@ -1,55 +1,100 @@
-@extends('Admin\partials\master')
+@extends('Admin.partials.master')
 @section('title')
-    {{ __('نمایش تگ منتخب') }}
+    {{ __('نمایش محصول منتخب') }}
+@endsection
+@section('scripts')
+    <script>
+        $("#tagSelect").selectpicker({
+            title : "نمایش تگ های منتخب"
+        })
+    </script>
 @endsection
 @section('content')
 
-<!-- Content Row -->
-<div class="row">
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-12 col-md-12 mb-4 bg-white my-4 p-4">
-        <div class="mb-4 d-flex">
-            <h5 class="font-weight-bold">
-                <span>{{ __('تگ') }}</span>
-            </h5>
-            <p> : {{ $tag->name }}</p>
-        </div>
-        <hr>
-        <div class="row">
-            {{-- show name --}}
-            <div class="col">
-                <label for="name">{{ __('نام تگ') }}</label>
-                <input type="text" disabled value="{{ $tag->name }} " class="form-control" />
+    <!-- Content Row -->
+    <div class="row">
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-12 col-md-12 mb-4 bg-white my-4 p-4">
+            <div class="mb-4">
+                <h5 class="font-weight-bold">
+                    {{ __('نمایش محصول') }} :
+                    {{ $product->name }}
+                </h5>
             </div>
-            {{-- end brand name --}}
+            <hr>
+            <div class="row">
 
-            {{-- show is active --}}
-            <div class="col">
-                <label for="is_active">{{ __('وضعیت') }}</label>
-                <select name="is_active" id="is_active" class="form-control form-select" disabled>
-                    <option selected >{{ $tag->is_active }}</option>
-                </select>
+                {{-- product title --}}
+                <div class="col-md-12">
+                    <h6 class="font-weight-bold">
+                        <i class="fas fa-store mx-2"></i>
+                        {{__('اطلاعات محصول')}}
+                    </h6>
+                </div>
+                {{-- End product title --}}
+
+                {{-- brand Product   --}}
+                <div class="col">
+                    <label for="name">{{ __('نام محصول') }}</label>
+                    <input class="form-control" value="{{ $product->name }}" disabled/>
+                </div>
+                {{-- end Product   --}}
+
+                {{-- Product brand_id --}}
+                <div class="col-3">
+                    <label for="BrandSelect">{{ __('برندها') }}</label>
+                    <select class="form-control" disabled>
+                        <option>{{ $product->brand->name }}</option>
+                    </select>
+                </div>
+                {{-- end Product brand_id --}}
+
+                {{-- Product is active --}}
+                <div class="col">
+                    <label for="is_active">{{ __('وضعیت') }}</label>
+                    <select class="form-control form-select" disabled>
+                        <option value="1" {{ $product->getRawOriginal('is_active') ? 'selected' : '' }}>فعال</option>
+                        <option value="0" {{ $product->getRawOriginal('is_active') ? '' : 'selected' }}>غیرفعال</option>
+                    </select>
+                </div>
+                {{-- end Product is active --}}
+
+                {{-- Product tag ids --}}
+                <div class="col-3">
+                    <label for="TagSelects">{{ __('تگ ها') }}</label>
+                    <select class="selectpicker form-control form-select" id="tagSelect" data-live-search="true">
+                        @foreach($product->tags as $tag)
+                            <option>{{ $tag->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                {{-- end Product tag ids --}}
+
+                {{-- description --}}
+                <div class="col-md-12 my-2">
+                    <label for="description">{{ __('توضیحات') }}</label>
+                    <textarea id="description" rows="3" class="form-control" disabled>{{ $product->description }}</textarea>
+                </div>
+                {{-- end description --}}
             </div>
-            {{-- end show is active --}}
-
-            {{-- created brand date --}}
-            <div class="col">
-                <label for="name">{{ __('تاریخ ایجاد') }}</label>
-                <input type="text" disabled value="{{ verta($tag->created_at)->format('l dS F | ساعت H:i') }} " class="form-control" />
+            <hr>
+            <div class="row">
+                <div class="col-md-12">
+                    <h6>
+                        <i class="fa fa-fw fa-pen mx-2"></i>
+                        <span>{{__('نمایش هزینه ارسال')}}</span>
+                        <span id="variationName" class="font-weight-bold"></span>
+                    </h6>
+                </div>
+                <div class="col-md-3">
+                    <label>هزینه ارسال</label>
+                    <input type="text" class="form-control" disabled value="{{ $product->delivery_amount }}">
+                </div>
+                <div class="col-md-3">
+                    <label>هزینه ارسال اضافی به ازای هر محصول</label>
+                    <input type="text" class="form-control" disabled value="{{ $product->delivery_amount_per_pro }}">
+                </div>
             </div>
-
-            {{-- updated brand date --}}
-            <div class="col">
-                <label for="name">{{ __('تاریخ ویرایش') }}</label>
-                <input type="text" disabled value="{{ verta($tag->updated_at)->format('l dS F | ساعت H:i') }} " class="form-control" />
-            </div>
-
-        </div>
-        <div class="my-4">
-            {{-- back button --}}
-            <a href="{{ route('admin.tags.index') }}" class="btn btn-dark mx-3">{{ __('بازگشت') }}</a>
         </div>
     </div>
-</div>
-
 @endsection

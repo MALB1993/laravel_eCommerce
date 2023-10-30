619ae62baabc54a463db8d5772cab61aca2d57bb
= <Carl.Alaeddin@gmail.com>
2023-10-30 09:00:13 +0330

    fixed products table

---
+++ database/migrations/2023_10_30_052054_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+
+            $table->foreignId('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->string('slug')->unique()->nullable();
+            $table->string('primary_image');
+            $table->text('description');
+            $table->integer('status')->default(1);
+            $table->boolean('is_active')->default(1);
+            $table->unsignedInteger('delivery_amount')->default(0);
+            $table->unsignedInteger('delivery_amount_per_product')->nullable(0);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};

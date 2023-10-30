32f82f4d1c55fbf2ba7166e62f61ba1d26614ab6
= <Carl.Alaeddin@gmail.com>
2023-10-30 09:28:09 +0330

    fixed product variations table

---
+++ database/migrations/2023_10_30_055328_create_product_variations_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('attribute_id');
+            $table->foreign('attribute_id')->references('id')->on('attributes')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->foreignId('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->string('value');
+            $table->unsignedInteger('price')->default(0);
+            $table->unsignedInteger('quantity')->default(0);
+            $table->string('sku')->nullable();
+
+            $table->unsignedInteger('sale_price')->nullable();
+
+            $table->timestamp('date_on_sale_from')->nullable();
+            $table->timestamp('date_on_to_from')->nullable();
+
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variations');
+    }
+};

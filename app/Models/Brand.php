6e73df94ef490b7aeebebd3f1aaae045b52f5607
= <Carl.Alaeddin@gmail.com>
2023-10-31 09:22:23 +0330

    added new trait sluggable and added abstruct functions for sluggable

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Brand extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, \Cviebrock\EloquentSluggable\Sluggable;
 
     protected $table = "brands";
 
@@ -18,4 +19,17 @@ class Brand extends Model
         "is_active"
     ];
 
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }

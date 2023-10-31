8068a6ea626f4ac81d691da044e49deff54f657c
= <Carl.Alaeddin@gmail.com>
2023-10-31 09:25:42 +0330

    create brand view and return create brand

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -20,7 +20,7 @@ class BrandController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.brands.create');
     }
 
     /**
---
+++ resources/views/admin/brands/create.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layouts.master')
+
+@section('title', __('create brands'))
+
+@section('content')
+
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('javascript')
+
+@endsection

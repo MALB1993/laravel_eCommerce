fba54f444e84eb4c6acc993a94016d2ef68199a3
= <Carl.Alaeddin@gmail.com>
2023-10-31 09:05:37 +0330

    fixed fillable and table

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Brand extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = "brands";
+
+    protected $fillable = [
+        "name",
+        "slug",
+        "is_active"
+    ];
+
 }

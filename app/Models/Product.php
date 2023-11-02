16d94802d4d7b7fc5196b262fd23430a264144ec
= <Carl.Alaeddin@gmail.com>
2023-11-02 14:33:26 +0330

    fixed table and fillable protected product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory , SoftDeletes;
+
+    protected $table = "products";
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'brand_id',
+        'category_id',
+        'primary_image',
+        'description',
+        'status',
+        'is_active',
+        'delivery_amount',
+        'delivery_amount_per_product'
+    ];
+
+    
+
 }

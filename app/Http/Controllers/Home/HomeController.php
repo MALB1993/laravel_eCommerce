c44faeca0b99a54ca6a23fc8197e772eeb0d7246
= <Carl.Alaeddin@gmail.com>
2023-11-08 11:22:13 +0330

    fixed banners indexs

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -10,9 +10,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $sliders = Banner::query()->where('type','LIKE','slider')->where('is_active','LIKE',1)->orderBy('priority')->get();
+        $sliders                = Banner::query()->where('type','LIKE','slider')->where('is_active','LIKE',1)->orderBy('priority')->get();
+        $index_banners_top      = Banner::query()->where('type','LIKE','index_banner_top')->where('is_active','LIKE',1)->orderBy('priority')->get();
+        $index_banners_bottom   = Banner::query()->where('type','LIKE','index_banner_bottom')->where('is_active','LIKE',1)->orderBy('priority')->get();
         return view('home.index',[
-            'sliders'   =>      $sliders
+            'sliders'               =>      $sliders,
+            'index_banners_top'     =>      $index_banners_top,
+            'index_banners_bottom'  =>      $index_banners_bottom
         ]);
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,7 @@
 @extends('home.layouts.master')
 
 @section('content')
+    {{-- sliders --}}
     <div class="slider-area section-padding-1">
         <div class="slider-active owl-carousel nav-style-1">
             @foreach ($sliders as $slider)
@@ -34,54 +35,34 @@
     <div class="banner-area pt-100 pb-65">
         <div class="container">
             <div class="row">
+                @foreach ($index_banners_top->chunk(3)->first() as $banner)
                 <div class="col-lg-4 col-md-4">
                     <div class="single-banner mb-30 scroll-zoom">
-                        <a href="product-details.html"><img class="animated" src="/home/assets/img/banner/banner-6.png"
-                                alt="" /></a>
+                        <a href="{{ $banner->button_link }}">
+                            <img class="animated" src="{{ asset(env('IMAGE_UPLOAD_PATH').'banners/'.$banner->image) }}" alt="{{ $banner->title }}" />
+                        </a>
                         <div class="banner-content-2 banner-position-5">
-                            <h4>زنانه</h4>
+                            <h4>{{ $banner->title }}</h4>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4">
-                    <div class="single-banner mb-30 scroll-zoom">
-                        <a href="product-details.html"><img class="animated" src="/home/assets/img/banner/banner-7.png"
-                                alt="" /></a>
-                        <div class="banner-content-2 banner-position-5">
-                            <h4>جین</h4>
+                @endforeach
+
+                @foreach ($index_banners_top->chunk(3)->last() as $banner)
+                    <div class="col-lg-6 col-md-6">
+                        <div class="single-banner mb-30 scroll-zoom">
+                            <a href="{{ $banner->button_link }}">
+                                <img class="animated" src="{{ asset(env('IMAGE_UPLOAD_PATH').'banners/'.$banner->image) }}" alt="{{ $banner->title }}" />
+                            </a>
+                            <div class="{{ $loop->last ? 'banner-content-3 banner-position-7' : 'banner-content banner-position-6 text-right' }}">
+                                <h3>{{ $banner->title }}</h3>
+                                <h2>{{ $banner->text }}</h2>
+                                <a href="{{ $banner->button_link }}">{{ $banner->button_text }}</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-4">
-                    <div class="single-banner mb-30 scroll-zoom">
-                        <a href="product-details.html"><img class="animated" src="/home/assets/img/banner/banner-8.png"
-                                alt="" /></a>
-                        <div class="banner-content-2 banner-position-5">
-                            <h4>مردانه</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6">
-                    <div class="single-banner mb-30 scroll-zoom">
-                        <a href="product-details.html"><img class="animated" src="/home/assets/img/banner/banner-9.png"
-                                alt="" /></a>
-                        <div class="banner-content banner-position-6 text-right">
-                            <h3>لورم ایپسوم</h3>
-                            <h2>لورم ایپسوم <br />متن</h2>
-                            <a href="product-details.html">فروشگاه</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6">
-                    <div class="single-banner mb-30 text-center scroll-zoom">
-                        <a href="product-details.html"><img class="animated" src="/home/assets/img/banner/banner-10.png"
-                                alt="" /></a>
-                        <div class="banner-content-3 banner-position-7">
-                            <h2>لورم ایپسوم متن ساختگی</h2>
-                            <a href="product-details.html">فروشگاه</a>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
+
             </div>
         </div>
     </div>
@@ -1599,28 +1580,20 @@
     <div class="banner-area pb-120">
         <div class="container">
             <div class="row">
+                @foreach ($index_banners_bottom as $banner)
                 <div class="col-lg-6 col-md-6 text-right">
                     <div class="single-banner mb-30 scroll-zoom">
-                        <a href="product-details.html"><img src="/home/assets/img/banner/banner-4.png"
-                                alt="" /></a>
-                        <div class="banner-content banner-position-3">
-                            <h3>لورم ایپسوم</h3>
-                            <h2>لورم ایپسوم <br />متن </h2>
-                            <a href="product-details.html">فروشگاه</a>
+                        <a href="product-details.html">
+                            <img src="{{ asset(env('IMAGE_UPLOAD_PATH').'banners/'.$banner->image) }}" alt="{{ $banner->title }}" />
+                        </a>
+                        <div class="{{ $loop->last ? 'banner-content banner-position-3' : 'banner-content banner-position-4' }}">
+                            <h3>{{ $banner->title }}</h3>
+                            <h2>{{ $banner->text }}</h2>
+                            <a href="{{ $banner->button_link }}">{{ $banner->button_text }}</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 text-right">
-                    <div class="single-banner mb-30 scroll-zoom">
-                        <a href="product-details.html"><img src="/home/assets/img/banner/banner-5.png"
-                                alt="" /></a>
-                        <div class="banner-content banner-position-4">
-                            <h3>لورم ایپسوم</h3>
-                            <h2>لورم ایپسوم </h2>
-                            <a href="product-details.html">فروشگاه</a>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>

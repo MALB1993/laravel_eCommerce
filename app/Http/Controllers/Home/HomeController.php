7b8e6799e7ff8bdc087d31848570923484b5cb88
= <Carl.Alaeddin@gmail.com>
2023-11-08 10:29:57 +0330

    fixed sliders

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $sliders = Banner::query()->where('type','LIKE','slider')->where('is_active','LIKE',1)->orderBy('priority')->get();
+        return view('home.index',[
+            'sliders'   =>      $sliders
+        ]);
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,60 +3,31 @@
 @section('content')
     <div class="slider-area section-padding-1">
         <div class="slider-active owl-carousel nav-style-1">
+            @foreach ($sliders as $slider)
             <div class="single-slider slider-height-1 bg-paleturquoise">
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6 text-right">
                             <div class="slider-content slider-animated-1">
-                                <h1 class="animated">لورم ایپسوم</h1>
-                                <p class="animated">
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
-                                    و با استفاده از طراحان گرافیک است
-                                </p>
+                                <h1 class="animated">{{ $slider->title }}</h1>
+                                <p class="animated">{{ $slider->text }}</p>
                                 <div class="slider-btn btn-hover">
-                                    <a class="animated" href="shop.html">
-                                        <i class="sli sli-basket-loaded"></i>
-                                        فروشگاه
+                                    <a class="animated" href="{{ $slider->button_link }}">
+                                        <i class="{{ $slider->button_icon }}"></i>
+                                        {{ $slider->button_text }}
                                     </a>
                                 </div>
                             </div>
                         </div>
                         <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
                             <div class="slider-single-img slider-animated-1">
-
-                                <img class="animated" src="/home/assets/img/slider/1.jfif" alt="" />
+                                <img class="animated" src="{{ asset(env('IMAGE_UPLOAD_PATH').'banners/'.$slider->image) }}" alt="{{ $slider->title }}" />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="single-slider slider-height-1 bg-paleturquoise">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6  text-right">
-                            <div class="slider-content slider-animated-1">
-                                <h1 class="animated">لورم ایپسوم</h1>
-                                <p class="animated">
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
-                                    و با استفاده از طراحان گرافیک است
-                                </p>
-                                <div class="slider-btn btn-hover">
-                                    <a class="animated" href="shop.html">
-                                        <i class="sli sli-basket-loaded"></i>
-                                        فروشگاه
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
-                            <div class="slider-single-img slider-animated-1">
-
-                                <img class="animated" src="/home/assets/img/slider/2.jfif" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 

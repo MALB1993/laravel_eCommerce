27579bc6f26c919edc0098a9ed6833ab73e69ef7
= <Carl.Alaeddin@gmail.com>
2023-11-11 12:23:50 +0330

    added new relation

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\ProductAttribute;
+use App\Models\ProductVariation;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -29,5 +31,24 @@ class Attribute extends Model
     public function categories()
     {
         return $this->belongsToMany(Category::class, 'attribute_category');
+    }
+
+    /**
+     * Summary of values
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function values()
+    {
+        return $this->hasMany(ProductAttribute::class)->select('attribute_id','value')->distinct();
+    }
+
+
+    /**
+     * Summary of values
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function variationsValues()
+    {
+        return $this->hasMany(ProductVariation::class)->select('attribute_id','value')->distinct();
     }
 }

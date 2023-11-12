f2161ad1ee7e8a310920eae1f736c8e20b64cb8f
= <Carl.Alaeddin@gmail.com>
2023-11-12 10:39:59 +0330

    fixed category controller

--- app/Http/Controllers/Home/CategoryController.php
+++ app/Http/Controllers/Home/CategoryController.php
@@ -9,15 +9,20 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
 
-    public function show(Category $category)
+    public function show(Request $request, Category $category)
     {
+
+
         $attributes = $category->attributes()->where('is_filter','LIKE',1)->with('values')->get();
         $variation = $category->attributes()->where('is_variation','LIKE',1)->with('variationsValues')->first();
+
+        $products = $category->products()->filter()->search()->paginate(9,['*'],'products');
 
         return view('home.categories.show',[
             'category'      =>  $category,
             'attributes'    =>  $attributes,
-            'variation'     =>  $variation
+            'variation'     =>  $variation,
+            'products'      =>  $products
         ]);
     }
 }

e46f4b9e7bb2d9d7941ce2cfa5b4eed8016b5ea3
= <Carl.Alaeddin@gmail.com>
2023-11-13 12:06:59 +0330

    added sweet alert cdn

--- resources/views/home/layouts/master.blade.php
+++ resources/views/home/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" lang="zxx">
+<html class="no-js" lang="fa" dir="ltr">
 
 <head>
     <meta charset="utf-8" />
@@ -8,21 +8,21 @@
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="/home/assets/img/favicon.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/home/assets/img/favicon.png') }}" />
 
     <!-- CSS
 ============================================ -->
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/home/assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ asset('/home/assets/css/bootstrap.min.css') }}" />
     <!-- Icon Font CSS -->
-    <link rel="stylesheet" href="/home/assets/css/icons.min.css" />
+    <link rel="stylesheet" href="{{ asset('/home/assets/css/icons.min.css') }}" />
     <!-- Plugins CSS -->
-    <link rel="stylesheet" href="/home/assets/css/plugins.css" />
+    <link rel="stylesheet" href="{{ asset('/home/assets/css/plugins.css') }}" />
     <!-- Main Style CSS -->
-    <link rel="stylesheet" href="/home/assets/css/style.css" />
+    <link rel="stylesheet" href="{{ asset('/home/assets/css/style.css') }}" />
     <!-- Modernizer JS -->
-    <!-- <script src="/home/assets/js/vendor/modernizr-2.8.3.min.js"></script> -->
+     <script src="{{ asset('/home/assets/js/vendor/modernizr-2.8.3.min.js') }}"></script>
     @yield('stylesheet')
 </head>
 
@@ -44,17 +44,19 @@
 ============================================ -->
 
     <!-- jQuery JS -->
-    <script src="/home/assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="{{ asset('/home/assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
     <!-- Popper JS -->
-    <script src="/home/assets/js/popper.min.js"></script>
+    <script src="{{ asset('/home/assets/js/popper.min.js') }}"></script>
     <!-- Bootstrap JS -->
-    <script src="/home/assets/js/bootstrap.min.js"></script>
+    <script src="{{ asset('/home/assets/js/bootstrap.min.js') }}"></script>
     <!-- Plugins JS -->
-    <script src="/home/assets/js/plugins.js"></script>
+    <script src="{{ asset('/home/assets/js/plugins.js') }}"></script>
     <!-- Ajax Mail -->
-    <script src="/home/assets/js/ajax-mail.js"></script>
+    <script src="{{ asset('/home/assets/js/ajax-mail.js') }}"></script>
     <!-- Main JS -->
-    <script src="/home/assets/js/main.js"></script>
+    <script src="{{ asset('/home/assets/js/main.js') }}"></script>
+
+    @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
     @yield('javascript')
 </body>
 

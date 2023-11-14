1556036cbdb1570f46df072be0e329f75c3a466c
= <Carl.Alaeddin@gmail.com>
2023-11-14 13:04:25 +0330

    fixed wishlist profile

--- app/Http/Controllers/Home/UserProfileController.php
+++ app/Http/Controllers/Home/UserProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
 
 class UserProfileController extends Controller
@@ -19,6 +20,14 @@ class UserProfileController extends Controller
         $comments = Comment::query()->where([['user_id',auth()->id()],['approved',1]])->get();
         return view('home.users_profile.comments',[
             'comments'   =>  $comments
+        ]);
+    }
+
+    public function wishlist()
+    {
+        $wishlists = Wishlist::query()->where('user_id',auth()->user()->id)->get();
+        return view('home.users_profile.wishlist',[
+            'wishlists'  => $wishlists
         ]);
     }
 

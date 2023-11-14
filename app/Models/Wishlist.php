62e7ea4e03b7d44c0ebde26413d8016bc5883990
= <Carl.Alaeddin@gmail.com>
2023-11-14 12:46:47 +0330

    fixed whishlist

---
+++ app/Models/Wishlist.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wishlist extends Model
+{
+    use HasFactory;
+
+    protected $table = 'wishlist';
+
+    protected $fillable = [
+        'user_id',
+        'product_id'
+    ];
+
+}

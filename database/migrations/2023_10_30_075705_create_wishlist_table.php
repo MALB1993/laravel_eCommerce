0c1d18ba45977eede49a75b035c22a628dc13197
= <Carl.Alaeddin@gmail.com>
2023-11-14 12:45:07 +0330

    modidifed : database wishlist

--- database/migrations/2023_10_30_075705_create_wishlist_table.php
+++ database/migrations/2023_10_30_075705_create_wishlist_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete()->cascadeOnUpdate();
 
             $table->primary(['user_id','product_id']);
-            
+
             $table->softDeletes();
             $table->timestamps();
         });

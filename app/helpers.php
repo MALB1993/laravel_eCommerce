bdc93884a99edb4206c794d07ac8689faa2bc137
= <Carl.Alaeddin@gmail.com>
2023-11-16 22:05:08 +0330

    fixed healpers bug

--- app/helpers.php
+++ app/helpers.php
@@ -1,30 +1,57 @@
 <?php
+
 use Carbon\Carbon;
 use Hekmatinasser\Verta\Facades\Verta;
 
+if (!function_exists('generateFileName')) {
+    function generateFileName($name)
+    {
+        $year         = Carbon::now()->year;
+        $month        = Carbon::now()->month;
+        $hour         = Carbon::now()->hour;
+        $minute       = Carbon::now()->minute;
+        $second       = Carbon::now()->second;
+        $microSecond  = Carbon::now()->microsecond;
 
-function generateFileName($name)
-{
-    $year         = Carbon::now()->year;
-    $month        = Carbon::now()->month;
-    $hour         = Carbon::now()->hour;
-    $minute       = Carbon::now()->minute;
-    $second       = Carbon::now()->second;
-    $microSecond  = Carbon::now()->microsecond;
+        return $year . "_" . $month . "_" . $hour . "_" . $minute . "_" . $second . "_" . $microSecond . "_" . $name;
+    }
+}
 
-    return $year ."_". $month ."_". $hour ."_". $minute ."_". $second ."_". $microSecond ."_". $name;
+if (!function_exists('convertShamsiToGeographical')) {
+    function convertShamsiToGeographical($shamsi)
+    {
+        if ($shamsi == null) {
+            return null;
+        }
+
+        $pattern = "/[-\s]/";
+        $shamsiDate = preg_split($pattern, $shamsi);
+        $date = $shamsiDate[0] . "-" . $shamsiDate[1] . "-" . $shamsiDate[2] . " " . $shamsiDate[3];
+        return Verta::parse($date)->datetime();
+    }
 }
 
 
-function convertShamsiToGeographical($shamsi)
-{
-    if($shamsi == null)
+if (!function_exists('cartTotalSaleAmount')) {
+    function cartTotalSaleAmount()
     {
-        return null;
+        $cartTotalSaleAmount = 0;
+        foreach (\Cart::getContent() as $item) {
+            if ($item->attributes->is_sale) {
+                $cartTotalSaleAmount += $item->quantity * ($item->attributes->price - $item->attributes->sale_price);
+            }
+        }
+        return $cartTotalSaleAmount;
     }
+}
 
-    $pattern = "/[-\s]/";
-    $shamsiDate = preg_split($pattern, $shamsi);
-    $date = $shamsiDate[0]."-".$shamsiDate[1]."-".$shamsiDate[2]." ".$shamsiDate[3];
-    return Verta::parse($date)->datetime();
+if (!function_exists('totalDeliveryAmount')) {
+    function totalDeliveryAmount()
+    {
+        $totalDeliveryAmount = 0;
+        foreach (\Cart::getContent() as $item) {
+            $totalDeliveryAmount += $item->associatedModel->delivery_amount;
+        }
+        return $totalDeliveryAmount;
+    }
 }

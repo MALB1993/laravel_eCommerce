79c9d517ba2126d1e05a7046e4fcda9bcbe0e696
= <Carl.Alaeddin@gmail.com>
2023-11-18 12:31:11 +0330

    fixed route for coupon

--- routes/web.php
+++ routes/web.php
@@ -92,7 +92,9 @@ Route::prefix('/')->name('home.')->group(function(){
     Route::post('/add-to-cart',[CartController::class,'add'])->name('cart-add');
     Route::get('/remove-form-cart/{rowId}',[CartController::class,'remove'])->name('cart.remove');
     Route::get('/clear-cart',[CartController::class,'clear'])->name('cart.clear');
-
+    
+    //================================== coupons
+    Route::post('/check-coupon',[CartController::class,'checkCoupon'])->name('check-coupon');
 
 });
 

e7adbfc884672d6cd6c64f9fdb422fc829bb42c4
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-21 08:34:35 +0330

    check user profile controller

--- app/Http/Controllers/Home/UserProfileController.php
+++ app/Http/Controllers/Home/UserProfileController.php
@@ -5,17 +5,27 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use App\Models\Wishlist;
+use Illuminate\Contracts\Foundation\Application as FoundationApplication;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 class UserProfileController extends Controller
 {
-    public function index()
+    /**
+     * @return View|Application|Factory|FoundationApplication
+     */
+    public function index(): View|Application|Factory|FoundationApplication
     {
         return view('home.users_profile.index');
     }
 
 
-    public function comment()
+    /**
+     * @return View|Application|Factory|FoundationApplication
+     */
+    public function comment(): View|Application|Factory|FoundationApplication
     {
         $comments = Comment::query()->where([['user_id',auth()->id()],['approved',1]])->get();
         return view('home.users_profile.comments',[
@@ -23,7 +33,10 @@ class UserProfileController extends Controller
         ]);
     }
 
-    public function wishlist()
+    /**
+     * @return View|Application|Factory|FoundationApplication
+     */
+    public function wishlist(): View|Application|Factory|FoundationApplication
     {
         $wishlists = Wishlist::query()->where('user_id',auth()->user()->id)->get();
         return view('home.users_profile.wishlist',[

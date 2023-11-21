c2d26b3e6c90af7bc06dc4db7582b552c486d276
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-21 08:33:37 +0330

    check product controller

--- app/Http/Controllers/Home/ProductController.php
+++ app/Http/Controllers/Home/ProductController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application as FoundationApplication;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function show(Product $product)
+    /**
+     * @param Product $product
+     * @return View|Application|Factory|FoundationApplication
+     */
+    public function show(Product $product): View|Application|Factory|FoundationApplication
     {
         return view('home.products.show',[
             'product'   =>    $product

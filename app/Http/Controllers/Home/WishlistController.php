e6a8e67dc9b8d0b6e14bbddfbdac432b1160eee5
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-21 08:36:03 +0330

    check whishlist controller

--- app/Http/Controllers/Home/WishlistController.php
+++ app/Http/Controllers/Home/WishlistController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Wishlist;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class WishlistController extends Controller
 {
-    public function add(Product $product)
+    /**
+     * @param Product $product
+     * @return RedirectResponse
+     */
+    public function add(Product $product): \Illuminate\Http\RedirectResponse
     {
 
         if(auth()->check())
@@ -17,40 +22,41 @@ class WishlistController extends Controller
             if($product->checkUserWishlist(auth()->user()->id))
             {
                 alert()->warning(__('Info'),__('You have this product in your favorites list.'));
-                return redirect()->back();
             }else{
-                Wishlist::create([
+                Wishlist::query()->create([
                     'user_id'       =>  auth()->id(),
                     'product_id'    =>  $product->id
                 ]);
                 alert()->success(__('Confirm'),__('The desired product has been correctly added to your wish list'));
-                return redirect()->back();
             }
         }else{
             alert()->error(__('Info'),__('You must first login in the website'));
-            return redirect()->back();
         }
+        return redirect()->back();
     }
 
 
-    public function remove(Product $product)
+    /**
+     * @param Product $product
+     * @return RedirectResponse
+     */
+    public function remove(Product $product): RedirectResponse
     {
         if(auth()->check())
         {
-            $wishlist = Wishlist::where('product_id',$product->id)->where('user_id',auth()->user()->id)->firstOrFail();
+            $wishlist = Wishlist::query()->where('product_id',$product->id)->where('user_id',auth()->user()->id)->firstOrFail();
 
             if($wishlist)
             {
-                Wishlist::where('product_id',$product->id)->where('user_id',auth()->user()->id)->delete();
+                Wishlist::query()->where('product_id',$product->id)->where('user_id',auth()->user()->id)->delete();
             }
 
             alert()->success(__('Confirm'),__('The desired product has been correctly remove to your wish list'));
-            return redirect()->back();
 
         }else{
             alert()->error(__('Info'),__('You must first login in the website'));
-            return redirect()->back();
         }
+        return redirect()->back();
     }
 
 }

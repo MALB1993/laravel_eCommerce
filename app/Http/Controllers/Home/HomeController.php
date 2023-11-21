aeb8dac611417317249d3432821542fdae93ddf1
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-21 08:32:35 +0330

    check home controller

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
 use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application as FoundationApplication;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    /**
+     * @return Application|Factory|View|FoundationApplication
+     */
+    public function index(): FoundationApplication|View|Factory|Application
     {
         $sliders                = Banner::query()->where('type','LIKE','sliders')->where('is_active','LIKE',1)->orderBy('priority')->get();
         $index_banners_top      = Banner::query()->where('type','LIKE','index_banner_top')->where('is_active','LIKE',1)->orderBy('priority')->get();

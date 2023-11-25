6b30ef46313e8f6c15f0ad1c56db3b95bd33d9f9
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-25 12:22:09 +0330

    fixed order item and set relations

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Product;
 
 class OrderItem extends Model
 {
@@ -20,5 +21,10 @@ class OrderItem extends Model
         'quantity',
         'subtotals'
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 
 }

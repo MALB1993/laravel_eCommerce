35d7955da5d5da507f565f0f6671426bf475c4af
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-26 12:46:53 +0330

    fixed sidebar

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -29,6 +29,22 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseUsers" aria-expanded="true" aria-controls="collapseUsers">
+            <i class="fas fa-fw fa-users"></i>
+            <span>{{ __('Users') }}</span>
+        </a>
+        <div id="collapseUsers" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">{{ __('Client') }}</h6>
+                <a class="collapse-item" href="{{ route('admin-panel.users.index') }}">لیست کاربران</a> 
+                <a class="collapse-item" href="">گروه های کاربری</a>
+                <a class="collapse-item" href="">دسترسی ها</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
         <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseProducts" aria-expanded="true" aria-controls="collapseProducts">
             <i class="fas fa-fw fa-cart-plus"></i>
             <span>{{ __('Products') }}</span>

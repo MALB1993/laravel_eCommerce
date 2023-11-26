732f59ec567683bdab4bcee2d30d5bbec2cb2e5c
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-26 10:24:08 +0330

    fixed transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\User;
+use App\Models\order;
 
 class Transaction extends Model
 {
@@ -23,4 +25,19 @@ class Transaction extends Model
         'status'
     ];
 
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getStatusAttribute($status)
+    {
+        return $status ? __('Paying success') : __('Paying error');
+    }
 }

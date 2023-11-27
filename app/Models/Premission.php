37979073599572caa10a37bdfd639f8b1ef93404
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-27 09:09:34 +0330

    fixed premission model

---
+++ app/Models/Premission.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
+
+class Premission extends Model
+{
+    use HasFactory, HasRoles;
+
+    protected $table = 'permissions';
+
+    protected $fillable = [
+        'name',
+        'display_name',
+        'guard_name'
+    ];
+
+}

3e485e7ee8dc957276b4961641ce65303de61ec9
CarlAlaeddin <Carl.Alaeddin@gmail.com>
2023-11-27 11:31:24 +0330

    fixed role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
+
+    protected $table = "roles";
+
+    protected $fillable = [
+        'name',
+        'display_name',
+        'guard_name'
+    ];
 }
